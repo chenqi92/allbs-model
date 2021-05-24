5fb3bf8e48a02c18d119a4a89a457b91d9de1fa9
cq <cq92104@163.com>
2021-05-24 14:46:18 +0800

    坐标系转换工具

---
+++ src/main/java/cn/allbs/utils/GPSConverterUtils.java
@@ -0,0 +1,183 @@
+package cn.allbs.utils;
+
+import lombok.experimental.UtilityClass;
+
+/**
+ * 百度坐标（BD09）、国测局坐标（火星坐标，GCJ02）、和WGS84坐标系之间的转换的工具
+ * <p>
+ * WGS84: Google Earth采用，Google Map中国范围外使用
+ * GCJ02: 火星坐标系，中国国家测绘局制定的坐标系统，由WGS84机密后的坐标。Google Map中国和搜搜地图使用，高德
+ * BD09:百度坐标，GCJ02机密后的坐标系
+ *
+ * @author ChenQi
+ */
+@UtilityClass
+public class GPSConverterUtils {
+
+    /**
+     * 地球半径
+     */
+    private static final double a = 6378137.0D;
+
+    static double x_pi = Math.PI * 3000.0 / 180.0;
+    /**
+     * 扁率
+     */
+    static double ee = 0.00669342162296594323;
+
+    /**
+     * 百度坐标系(BD-09)转WGS坐标
+     *
+     * @param lng 百度坐标纬度
+     * @param lat 百度坐标经度
+     * @return WGS84坐标数组
+     */
+    public static double[] bd09towgs84(double lng, double lat) {
+        double[] gcj = bd09togcj02(lng, lat);
+        double[] wgs84 = gcj02towgs84(gcj[0], gcj[1]);
+        return wgs84;
+    }
+
+    /**
+     * WGS坐标转百度坐标系(BD-09)
+     *
+     * @param lng WGS84坐标系的经度
+     * @param lat WGS84坐标系的纬度
+     * @return 百度坐标数组
+     */
+    public static double[] wgs84tobd09(double lng, double lat) {
+        double[] gcj = wgs84togcj02(lng, lat);
+        double[] bd09 = gcj02tobd09(gcj[0], gcj[1]);
+        return bd09;
+    }
+
+    /**
+     * 火星坐标系(GCJ-02)转百度坐标系(BD-09)
+     * <p>
+     * 谷歌、高德——>百度
+     *
+     * @param lng 火星坐标经度
+     * @param lat 火星坐标纬度
+     * @return 百度坐标数组
+     */
+    public static double[] gcj02tobd09(double lng, double lat) {
+        double z = Math.sqrt(lng * lng + lat * lat) + 0.00002 * Math.sin(lat * x_pi);
+        double theta = Math.atan2(lat, lng) + 0.000003 * Math.cos(lng * x_pi);
+        double bd_lng = z * Math.cos(theta) + 0.0065;
+        double bd_lat = z * Math.sin(theta) + 0.006;
+        return new double[]{bd_lng, bd_lat};
+    }
+
+    /**
+     * 百度坐标系(BD-09)转火星坐标系(GCJ-02)
+     * <p>
+     * 百度——>谷歌、高德
+     *
+     * @param bd_lon 百度坐标纬度
+     * @param bd_lat 百度坐标经度
+     * @return 火星坐标数组
+     */
+    public static double[] bd09togcj02(double bd_lon, double bd_lat) {
+        double x = bd_lon - 0.0065;
+        double y = bd_lat - 0.006;
+        double z = Math.sqrt(x * x + y * y) - 0.00002 * Math.sin(y * x_pi);
+        double theta = Math.atan2(y, x) - 0.000003 * Math.cos(x * x_pi);
+        double gg_lng = z * Math.cos(theta);
+        double gg_lat = z * Math.sin(theta);
+        return new double[]{gg_lng, gg_lat};
+    }
+
+    /**
+     * WGS84转GCJ02(火星坐标系)
+     *
+     * @param lng WGS84坐标系的经度
+     * @param lat WGS84坐标系的纬度
+     * @return 火星坐标数组
+     */
+    public static double[] wgs84togcj02(double lng, double lat) {
+        if (out_of_china(lng, lat)) {
+            return new double[]{lng, lat};
+        }
+        double dlat = transformlat(lng - 105.0, lat - 35.0);
+        double dlng = transformlng(lng - 105.0, lat - 35.0);
+        double radlat = lat / 180.0 * Math.PI;
+        double magic = Math.sin(radlat);
+        magic = 1 - ee * magic * magic;
+        double sqrtmagic = Math.sqrt(magic);
+        dlat = (dlat * 180.0) / ((a * (1 - ee)) / (magic * sqrtmagic) * Math.PI);
+        dlng = (dlng * 180.0) / (a / sqrtmagic * Math.cos(radlat) * Math.PI);
+        double mglat = lat + dlat;
+        double mglng = lng + dlng;
+        return new double[]{mglng, mglat};
+    }
+
+    /**
+     * GCJ02(火星坐标系)转GPS84
+     *
+     * @param lng 火星坐标系的经度
+     * @param lat 火星坐标系纬度
+     * @return WGS84坐标数组
+     */
+    public static double[] gcj02towgs84(double lng, double lat) {
+        if (out_of_china(lng, lat)) {
+            return new double[]{lng, lat};
+        }
+        double dlat = transformlat(lng - 105.0, lat - 35.0);
+        double dlng = transformlng(lng - 105.0, lat - 35.0);
+        double radlat = lat / 180.0 * Math.PI;
+        double magic = Math.sin(radlat);
+        magic = 1 - ee * magic * magic;
+        double sqrtmagic = Math.sqrt(magic);
+        dlat = (dlat * 180.0) / ((a * (1 - ee)) / (magic * sqrtmagic) * Math.PI);
+        dlng = (dlng * 180.0) / (a / sqrtmagic * Math.cos(radlat) * Math.PI);
+        double mglat = lat + dlat;
+        double mglng = lng + dlng;
+        return new double[]{lng * 2 - mglng, lat * 2 - mglat};
+    }
+
+    /**
+     * 纬度转换
+     *
+     * @param lng 经度
+     * @param lat 纬度
+     * @return 纬度转换值
+     */
+    public static double transformlat(double lng, double lat) {
+        double ret = -100.0 + 2.0 * lng + 3.0 * lat + 0.2 * lat * lat + 0.1 * lng * lat + 0.2 * Math.sqrt(Math.abs(lng));
+        ret += (20.0 * Math.sin(6.0 * lng * Math.PI) + 20.0 * Math.sin(2.0 * lng * Math.PI)) * 2.0 / 3.0;
+        ret += (20.0 * Math.sin(lat * Math.PI) + 40.0 * Math.sin(lat / 3.0 * Math.PI)) * 2.0 / 3.0;
+        ret += (160.0 * Math.sin(lat / 12.0 * Math.PI) + 320 * Math.sin(lat * Math.PI / 30.0)) * 2.0 / 3.0;
+        return ret;
+    }
+
+    /**
+     * 经度转换
+     *
+     * @param lng 经度
+     * @param lat 纬度
+     * @return 经度转换值
+     */
+    public static double transformlng(double lng, double lat) {
+        double ret = 300.0 + lng + 2.0 * lat + 0.1 * lng * lng + 0.1 * lng * lat + 0.1 * Math.sqrt(Math.abs(lng));
+        ret += (20.0 * Math.sin(6.0 * lng * Math.PI) + 20.0 * Math.sin(2.0 * lng * Math.PI)) * 2.0 / 3.0;
+        ret += (20.0 * Math.sin(lng * Math.PI) + 40.0 * Math.sin(lng / 3.0 * Math.PI)) * 2.0 / 3.0;
+        ret += (150.0 * Math.sin(lng / 12.0 * Math.PI) + 300.0 * Math.sin(lng / 30.0 * Math.PI)) * 2.0 / 3.0;
+        return ret;
+    }
+
+    /**
+     * 判断是否在国内，不在国内不做偏移
+     *
+     * @param lng 经度
+     * @param lat 纬度
+     * @return true 国外 false 国内
+     */
+    public static boolean out_of_china(double lng, double lat) {
+        if (lng < 72.004 || lng > 137.8347) {
+            return true;
+        } else if (lat < 0.8293 || lat > 55.8271) {
+            return true;
+        }
+        return false;
+    }
+}
--- src/main/java/cn/allbs/utils/LngLatUtil.java
+++ src/main/java/cn/allbs/utils/LngLatUtil.java
@@ -2,10 +2,16 @@ package cn.allbs.utils;
 
 import cn.allbs.constant.CommonConstant;
 import cn.hutool.core.util.NumberUtil;
+import cn.hutool.json.JSONArray;
 import lombok.experimental.UtilityClass;
 
+import java.awt.*;
+import java.awt.geom.GeneralPath;
+import java.awt.geom.Point2D;
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -99,6 +105,7 @@ public class LngLatUtil {
         double s = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(a / 2), 2) +
                 Math.cos(radLat1) * Math.cos(radLat2) * Math.pow(Math.sin(b / 2), 2)));
         s = s * WGS84_EARTH_RADIUS;
+        s = Math.round(s * 10000d) / 10000d;
         return s;
     }
 
@@ -127,5 +134,67 @@ public class LngLatUtil {
         result.put(CommonConstant.LONGITUDE, NumberUtil.round(lng, 6).doubleValue());
         result.put(CommonConstant.LATITUDE, NumberUtil.round(lat, 6).doubleValue());
         return result;
+    }
+
+    /**
+     * 判断一个点是否在圆形区域内
+     *
+     * @param radius 半径
+     * @param lat1   圆心纬度
+     * @param lng1   圆心经度
+     * @param lat2   坐标纬度
+     * @param lng2   坐标经度
+     */
+    public static boolean isInCircle(double lng1, double lat1, double lng2, double lat2, String radius) {
+        return getDistance(lat1, lng1, lat2, lng2) > Double.parseDouble(radius);
+    }
+
+    /**
+     * 判断是否在多边形区域内
+     *
+     * @param pointLon 要判断的点的纵坐标
+     * @param pointLat 要判断的点的横坐标
+     * @param points   经纬度json数组 "[{\"x\":120.61123416,\"y\":31.32889074,\"z\":137.05},{\"x\":120.61312695,\"y\":31.31892631,\"z\":128.61},{\"x\":120.61455616,\"y\":31.30808702,\"z\":43.66},{\"x\":120.62127327,\"y\":31.30899876,\"z\":62.21},{\"x\":120.63003506,\"y\":31.31057071,\"z\":29.43},{\"x\":120.63726235,\"y\":31.31203339,\"z\":92.90},{\"x\":120.64536616,\"y\":31.31334188,\"z\":78.36},{\"x\":120.64402082,\"y\":31.31947999,\"z\":13.19},{\"x\":120.64136126,\"y\":31.32757908,\"z\":87.36},{\"x\":120.63689776,\"y\":31.33287239,\"z\":60.62},{\"x\":120.63502091,\"y\":31.33742080,\"z\":114.21},{\"x\":120.63071787,\"y\":31.33793104,\"z\":32.99},{\"x\":120.62952446,\"y\":31.34483170,\"z\":164.79},{\"x\":120.62710968,\"y\":31.34801804,\"z\":164.15},{\"x\":120.62731359,\"y\":31.34823458,\"z\":189.53},{\"x\":120.62700980,\"y\":31.34894193,\"z\":194.24},{\"x\":120.62700980,\"y\":31.34894193,\"z\":194.24},{\"x\":120.62700980,\"y\":31.34894193,\"z\":194.24},{\"x\":120.62665860,\"y\":31.34861797,\"z\":155.41},{\"x\":120.61706620,\"y\":31.34846463,\"z\":200.05},{\"x\":120.61854348,\"y\":31.34267516,\"z\":138.68},{\"x\":120.62111689,\"y\":31.33313042,\"z\":154.61}]"
+     * @return true 在范围内 false 不在范围内
+     */
+    public static boolean isInPolygon(double pointLon, double pointLat, JSONArray points) {
+        // 将要判断的横纵坐标组成一个点
+        Point2D.Double point = new Point.Double(pointLon, pointLat);
+        // 将区域各顶点的横纵坐标放到一个点集合里面
+        List<Point2D.Double> pointList = new ArrayList<>();
+        double polygonPointX, polygonPointY;
+        int len = points.size();
+        for (int i = 0; i < len; i++) {
+            polygonPointX = points.getJSONObject(i).getDouble("x");
+            polygonPointY = points.getJSONObject(i).getDouble("y");
+            Point2D.Double polygonPoint = new Point2D.Double(polygonPointX, polygonPointY);
+            pointList.add(polygonPoint);
+        }
+        return check(point, pointList);
+    }
+
+    /**
+     * 一个点是否在多边形内
+     *
+     * @param point   要判断的点的横纵坐标
+     * @param polygon 组成的顶点坐标集合
+     * @return true 在范围内 false 不在范围内
+     */
+    private static boolean check(Point2D.Double point, List<Point2D.Double> polygon) {
+        GeneralPath penalPath = new GeneralPath();
+
+        Point2D.Double first = polygon.get(0);
+        // 通过移动到指定坐标（以双精度指定），将一个点添加到路径中
+        penalPath.moveTo(first.x, first.y);
+        polygon.remove(0);
+        for (Point2D.Double d : polygon) {
+            // 通过绘制一条从当前坐标到新指定坐标（以双精度指定）的直线，将一个点添加到路径中。
+            penalPath.lineTo(d.x, d.y);
+        }
+        // 将几何多边形封闭
+        penalPath.lineTo(first.x, first.y);
+        penalPath.closePath();
+        // 测试指定的 Point2D 是否在 Shape 的边界内。
+        return penalPath.contains(point);
     }
 }
