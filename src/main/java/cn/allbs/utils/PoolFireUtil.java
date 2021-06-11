7215a93bfa36ddce704fb7263da0bce5fd361aa8
cq <cq92104@163.com>
2021-06-11 17:30:40 +0800

    池火灾完善

--- src/main/java/cn/allbs/utils/PoolFireUtil.java
+++ src/main/java/cn/allbs/utils/PoolFireUtil.java
@@ -13,12 +13,13 @@ public class PoolFireUtil {
     /**
      * 池火灾模型计算
      *
-     * @param s 观察者距液池中心的距离与火焰半径的比
-     * @param h 火焰高度与火焰半径的比
-     * @param x 目标储罐离液池中心的水平距离
-     * @param n 效率因子
-     * @param m 单位池面积质量燃烧率 kg/(m2·s)
-     * @param d 液池直径
+     * @param s  观察者距液池中心的距离与火焰半径的比
+     * @param h  火焰高度与火焰半径的比
+     * @param x  目标储罐离液池中心的水平距离
+     * @param n  效率因子
+     * @param m  单位池面积质量燃烧率 kg/(m2·s)
+     * @param d  液池直径
+     * @param hc 液体燃烧热，kJ/kg
      * @return 火焰表面平均辐射强度 kW/m2
      */
     public Double mudan(double s, double h, double x, double n, double m, double d, double hc) {
@@ -79,5 +80,51 @@ public class PoolFireUtil {
     public Double fireHeightWithoutWind(double m, double p0, double g, double d, double u, double pv) {
         double uf = u / Math.pow(g * m * d / pv, 1 / (float) 3);
         return 55 * Math.pow(m / (p0 * Math.sqrt(g * d)), 0.67) * Math.pow(uf, -0.21);
+    }
+
+    /**
+     * 无风时火焰表面平均辐射强度
+     *
+     * @param n  效率因子
+     * @param hc 液体燃烧热，kJ/kg
+     * @param x  目标储罐离液池中心的水平距离
+     * @param m  位池面积质量燃烧率 kg/(m2·s)
+     * @param p0 空气密度，kg/m3
+     * @param g  重力加速度，取9.8m/s2
+     * @param d  液池直径，m
+     * @return 火焰表面平均辐射强度 kW/m2
+     */
+    public Double countWithoutWind(double n, double hc, double x, double m, double p0, double g, double d) {
+        // 火焰可见高度m
+        double hei = fireHeightWithWind(m, p0, g, d);
+        // 火焰高度与火焰半径的比
+        double h = 2 * hei / d;
+        // 观察者距液池中心的距离与火焰半径的比
+        double s = 2 * x / d;
+        return mudan(s, h, x, n, m, d, hc);
+    }
+
+    /**
+     * 有风时火焰表面平均辐射强度
+     *
+     * @param n  效率因子
+     * @param hc 液体燃烧热，kJ/kg
+     * @param x  目标储罐离液池中心的水平距离
+     * @param m  位池面积质量燃烧率 kg/(m2·s)
+     * @param p0 空气密度，kg/m3
+     * @param g  重力加速度，取9.8m/s2
+     * @param d  液池直径，m
+     * @param u  10米高处的风速，m/s
+     * @param pv 可燃液体的蒸气密度，kg/m3
+     * @return 火焰表面平均辐射强度 kW/m2
+     */
+    public Double countWithWind(double n, double hc, double x, double m, double p0, double g, double d, double u, double pv) {
+        // 火焰可见高度m
+        double hei = fireHeightWithoutWind(m, p0, g, d, u, pv);
+        // 火焰高度与火焰半径的比
+        double h = 2 * hei / d;
+        // 观察者距液池中心的距离与火焰半径的比
+        double s = 2 * x / d;
+        return mudan(s, h, x, n, m, d, hc);
     }
 }
